533732272812f97cb9804a4642dafd4376d2030f
batiky <mhbtkhaydarova@gmail.com>
2022-03-05 10:17:51 -0500

    types of mapping

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/batiky/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.batiky.enums;
+
+public enum Gender {
+    MALE,FEMALE;
+}
---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/batiky/model/Mentor.java
@@ -0,0 +1,15 @@
+package com.batiky.model;
+
+import com.batiky.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Mentor {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private Gender Gender;
+
+}
